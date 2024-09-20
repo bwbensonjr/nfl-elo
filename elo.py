e9ae94c8d80317877919ee673156b7cbaa81b4dc
Brent Benson <125278138+bbenson-hbs@users.noreply.github.com>
2024-09-20 10:56:38 -0400

    First commit

---
+++ elo.py
@@ -0,0 +1,85 @@
+"""A simple and general-purpose Elo calculation
+class. The default values of `k`, `home_field`,
+`rating_mean`, and `point_spread_div` are
+optimized for the NFL and may need adjustment
+for other sports."""
+
+import math
+
+class Elo:
+    def __init__(
+            self,
+            teams=None,
+            k=20,
+            home_field=50,
+            rating_mean=1505,
+            point_spread_div=25,
+    ):
+        self.ratings = {}
+        self.k = k
+        self.home_field = home_field
+        self.rating_mean = rating_mean
+        self.point_spread_div = point_spread_div
+        if teams:
+            for team_name in teams:
+                self.add_team(team_name)
+
+    def add_team(self, team_name, initial_rating=1500):
+        self.ratings[team_name] = initial_rating
+
+    def team_rating(self, team_name):
+        rating = self.ratings[team_name]
+        return rating
+
+    def set_rating(self, team_name, new_rating):
+        self.ratings[team_name] = new_rating
+
+    def elo_difference(self, home_team, away_team):
+        home_elo = self.team_rating(home_team)
+        away_elo = self.team_rating(away_team)
+        elo_diff = away_elo - (home_elo + self.home_field)
+        return elo_diff
+
+    def home_win_prob(self, home_team, away_team):
+        elo_diff = self.elo_difference(home_team, away_team)
+        expected_home = 1 / (1 + 10 ** (elo_diff/400))
+        return expected_home
+    
+    def point_spread(self, home_team, away_team):
+        elo_diff = self.elo_difference(home_team, away_team)
+        spread = -(elo_diff / self.point_spread_div)
+        return spread
+    
+    def update_ratings(self, home_team, home_score, away_team, away_score):
+        if home_score > away_score:
+            result_home = 1
+        elif away_score > home_score:
+            result_home = 0
+        else:
+            result_home = 0.5
+            
+        expected_home = self.home_win_prob(home_team, away_team)
+        forecast_delta = result_home - expected_home
+
+        score_diff = abs(home_score - away_score)
+        score_multiplier = math.log(score_diff + 1)
+
+        elo_change = self.k * score_multiplier * forecast_delta
+        new_home_elo = self.team_rating(home_team) + elo_change
+        new_away_elo = self.team_rating(away_team) - elo_change
+        self.set_rating(home_team, new_home_elo)
+        self.set_rating(away_team, new_away_elo)
+
+        return new_home_elo, new_away_elo
+
+    def regress_towards_mean(self, regress_mult=0.33):
+        """Regress all of the ratings towards a historic mean.
+        This is usually done to reflect a variety of possible
+        team quality changes between seasons."""
+        for team in self.ratings:
+            old_rating = self.team_rating(team)
+            rating_adjustment = (
+                (self.rating_mean - old_rating) * regress_mult
+            )
+            new_rating = old_rating + rating_adjustment
+            self.set_rating(team, new_rating)
---
+++ nfl_elo.py
@@ -0,0 +1,92 @@
+from elo import Elo
+import pandas as pd
+
+GAME_CSV_URL = (
+    "https://raw.githubusercontent.com/nflverse/"
+    "nfldata/master/data/games.csv"
+)
+OUT_FILE = "nfl_latest_elo.csv"
+
+def main():
+    print(f"Reading games from {GAME_CSV_URL}...")
+    nfl_games = pd.read_csv(GAME_CSV_URL)
+    print(f"Retrieved {len(nfl_games)} games.")
+    nfl_teams = set(
+        list(nfl_games["home_team"].unique()) +
+        list(nfl_games["away_team"].unique())
+    )
+    print(f"Teams: {nfl_teams}")
+    nfl_elo = Elo(teams=nfl_teams)
+    games_with_elo = process_game_elo(nfl_elo, nfl_games)
+    print(f"Writing games with updated Elo to {OUT_FILE}...")
+    games_with_elo.to_csv("nfl_latest_elo.csv", index=False)
+    print("Done.")
+
+def process_game_elo(elo, games_input, verbose=False):
+    # Copy for output
+    games = games_input.copy()
+    for season in range(1999, 2025):
+        print(f"{season} Season...")
+        season_games = games[games["season"] == season]
+        for ix, game in season_games.iterrows():
+            if pd.isna(game["home_score"]):
+                # Predict upcoming game, rather than update model
+                home_team = game["home_team"]
+                away_team = game["away_team"]
+                away_elo = elo.team_rating(away_team)
+                home_elo = elo.team_rating(home_team)
+                home_win_prob = elo.home_win_prob(home_team, away_team)
+                away_win_prob = 1 - home_win_prob
+                point_spread = elo.point_spread(home_team, away_team)
+                if verbose:
+                    print(f"{game.season}, week {game.week}: "
+                          f"{away_team} ({away_elo:.0f} - {away_win_prob:.0%}) "
+                          f"{point_spread:.0f} at {home_team} ({home_elo:.0f} "
+                          f"- {home_win_prob:.0%})")
+                # Update model with game predictions
+                games.at[ix, "home_elo"] = home_elo
+                games.at[ix, "away_elo"] = away_elo
+                games.at[ix, "home_win_prob"] = home_win_prob
+                games.at[ix, "away_win_prob"] = away_win_prob
+                games.at[ix, "point_spread"] = point_spread
+            else:
+                # Update model with games results
+                home_elo, away_elo, home_elo_post, away_elo_post = update_elo(elo, game)
+                if verbose:
+                    print(f"{game.season}, week {game.week}: "
+                          f"{game.away_team} ({away_elo:.0f}-{away_elo_post:.0f}) "
+                          f"{game.away_score:.0f} at {game.home_team} ({home_elo:.0f} "
+                          f"- {home_elo_post:.0f}) {game.home_score:.0f}")
+                # Update row with Elo values
+                games.at[ix, "home_elo"] = home_elo
+                games.at[ix, "away_elo"] = away_elo
+                games.at[ix, "home_elo_post"] = home_elo_post
+                games.at[ix, "away_elo_post"] = away_elo_post
+        print(f"End of season {season}")
+        if season != 2024:
+            print("Regressing towards the mean between seasons...")
+            elo.regress_towards_mean()
+    return games
+            
+def update_elo(elo, game):
+    home_team = game["home_team"]
+    away_team = game["away_team"]
+    pre_home = elo.team_rating(home_team)
+    pre_away = elo.team_rating(away_team)
+    elo.update_ratings(
+        home_team,
+        game["home_score"],
+        away_team,
+        game["away_score"],
+    )
+    post_home = elo.team_rating(home_team)
+    post_away = elo.team_rating(away_team)
+    return (
+        pre_home,
+        pre_away,
+        post_home,
+        post_away,
+    )
+
+if __name__ == "__main__":
+    main()
